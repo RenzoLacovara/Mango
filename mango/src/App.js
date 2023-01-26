adcbac44a30d4364714e76adebefab6d0a985d23
Renzo Lacovara <rnlacovara@gmail.com>
2023-01-26 01:34:34 -0300

    tailwind config

---
+++ mango/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="bg-slate-700">
+      <h1 className="text-green-600 text-3xl p-8">Mango</h1>
+    </div>
+  );
+}
+
+export default App;
